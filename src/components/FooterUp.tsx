d4e9969b0ad4fb789137eff98406566ac33f8ef9
Adeyemi Esther <estheradeyemi796@gmail.com>
2025-11-22 18:15:12 +0100

    feat:added padding

--- src/components/FooterUp.tsx
+++ src/components/FooterUp.tsx
@@ -74,7 +74,7 @@ function FooterUp() {
 
             <button
               type="submit"
-              className="mt-2 w-full bg-[#7DE5F2] text-black font-semibold font-light px-4 py-3 my-2 mr-2 rounded-md hover:bg-[#7DE5F2] hover:text-black transition-colors duration-200 flex items-center justify-center text-center min-[400px]:hidden"
+              className="mt-2 w-full bg-[#7DE5F2] text-black font-semibold font-light px-4 py-4 my-2 mr-2 rounded-md hover:bg-[#7DE5F2] hover:text-black transition-colors duration-200 flex items-center justify-center text-center min-[400px]:hidden"
             >
               Join the waitlist
             </button>
