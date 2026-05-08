bc2a1b3f008c17bd627176cbbf3d3f30cf27ea23
Temitope Adebayo <stoneachi@gmail.com>
2026-05-08 11:21:28 +0100

    fix course type selection on iphone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import favicon from "./faviconnn.jpg";
 
 import { Space_Grotesk } from "next/font/google";
-import { Suspense } from "react";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -103,9 +102,7 @@ export default function RootLayout({
       <body suppressHydrationWarning>
         <Providers>
           <Toaster position="top-center" />
-          <Suspense>
-            {children}
-          </Suspense>
+          {children}
           <CookieConsent />
         </Providers>
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
