512973625c15c1de5725ff7ff66e2b6ed47f56ef
Tjoseph-O <mrtemitopejoseph@gmail.com>
2026-05-18 20:54:37 +0100

    update the policy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ const socialLinks = [
   // { label: "Telegram", href: "https://t.me/+c5zFTQKZjGFmNGRk" },
 ];
 
-const contactLinks = [{ label: "Contact us", href: "fluensyfrench@gmail.com" }];
+const contactLinks = [{ label: "Contact us", href: "bonjour@fluensyfrench.com" }];
 
 const Footer = () => {
   return (
